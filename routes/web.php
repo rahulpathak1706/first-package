18836dbe64d193ebf9522f41cf9b5d13af803f1e
Rahul Pathak <rahulsp@zignuts.com>
2023-12-07 18:24:05 +0530

    abc plus package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('plus', function(abc\plus\PlusClass $plus) {
+    return $plus->justDoIt();
+});
